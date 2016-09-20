20c4c3a82444eeb52630239271fb8813dc2d4a81
Samantha Grise <AndrewMartin@AndrewMartin.local>
2016-09-20 08:15:43 -0400

    Update index.php wit file level error reporting and connection to logic and xkcd_logic.php with initialized variables

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
+<?php
+	# Report Errors, Warnings, and Notices
+	error_reporting(E_ALL);       
+	# Display errors on page (instead of a log file)
+	ini_set('display_errors', 1); 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>xkcd Password Generator</title>
+	<?php require 'xkcd_logic.php'; ?>
 </head>
+
 <body>
 	<h1>xkcd Password Generator</h1>
 	<form>
--- xkcd_logic.php
+++ xkcd_logic.php
@@ -1,6 +1,24 @@
 <?php
-	$num_words = ;
-	$add_num = ;
-	$add_symbol = ;
-	$words = ;
-	$password = ;
+	# initialize $numWords to 1
+	$numWords = 1;
+	# initialize $addNum to FALSE
+	$addNum = FALSE;
+	# initialize $addSymbol to FALSE
+	$addSymbol = FALSE;
+	# create list of words for password
+	$wordsList = [
+		'apples',
+		'bananas',
+		'pears',
+		'cherries',
+		'peaches',
+		'strawberries',
+		'kiwis',
+		'oranges',
+		'lemons',
+		'pinneaples',
+		'lychees',
+		'blueberries'
+	];
+	# initialize $password to empty string
+	$password = '';
