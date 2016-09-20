11ad0122f2933f93f5470ce1b798b41754238b27
Samantha Grise <AndrewMartin@AndrewMartin.local>
2016-09-20 07:51:47 -0400

    Add index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>xkcd Password Generator</title>
+</head>
+<body>
+	<h1>xkcd Password Generator</h1>
+	<form>
+		
+	</form>
+</body>
+</html>
